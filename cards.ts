2f1f0fbd1a7de319414e964bac7b1d0acbc09401
D7724 <Ryan_Truttmann@sluz.ch>
2026-04-24 14:56:47 +0200

    Implement Card class and associated enums; add tests for instantiation and comparison logic

--- cards.ts
+++ cards.ts
@@ -1,3 +1,35 @@
-export function hello(): string {
-  return "world";
+export class Card {
+  color: Color;
+  value: Value;
+
+  constructor(color: Color, value: Value) {
+    this.color = color;
+    this.value = value;
+  }
+
+  public beats(card: Card): boolean {
+    if (this.value === card.value) {
+      return this.color > card.color;
+    }
+    return this.value > card.value;
+  }
+}
+
+export enum Color {
+  Rosen = 4,
+  Schellen = 3,
+  Schilten = 2,
+  Eicheln = 1,
+}
+
+export enum Value {
+  Ass = 14,
+  König = 13,
+  Ober = 12,
+  Under = 11,
+  Banner = 10,
+  Nine = 9,
+  Eight = 8,
+  Seven = 7,
+  Six = 6,
 }
--- cards_test.ts
+++ cards_test.ts
@@ -1,6 +1,22 @@
 import { assertEquals } from "@std/assert";
-import { hello } from "./cards.ts";
+import { Card, Color, Value } from "./cards.ts";
 
-Deno.test("hello world", () => {
-  assertEquals(hello(), "world");
+Deno.test("Card class should be instantiated correctly", () => {
+  const card = new Card(Color.Rosen, Value.Ass);
+  assertEquals(card.color, Color.Rosen);
+  assertEquals(card.value, Value.Ass);
+});
+
+Deno.test("Card beats another card by value", () => {
+  const card1 = new Card(Color.Schellen, Value.Ass);
+  const card2 = new Card(Color.Schellen, Value.König);
+  const result = card1.beats(card2);
+  assertEquals(result, true);
+});
+
+Deno.test("Card beats another card by color", () => {
+  const card1 = new Card(Color.Schellen, Value.Ass);
+  const card2 = new Card(Color.Eicheln, Value.Ass);
+  const result = card1.beats(card2);
+  assertEquals(result, true);
 });
